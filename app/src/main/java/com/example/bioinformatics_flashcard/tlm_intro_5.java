fe51b75529b54ba4c2a2d9706e4dd8124a2d1e72
Daniel Nelson <danielnelsoenarto17@gmail.com>
2023-01-05 17:48:56 +0700

    second commit: until tlm 5

--- app/src/main/java/com/example/bioinformatics_flashcard/tlm_intro_5.java
+++ app/src/main/java/com/example/bioinformatics_flashcard/tlm_intro_5.java
@@ -6,21 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class tlm_intro_4 extends AppCompatActivity {
+public class tlm_intro_5 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tlm_intro4);
+        setContentView(R.layout.activity_tlm_intro5);
     }
-
-    public void launch_TLM_3(View v){
-        Intent i = new Intent(this, tlm_intro_3.class);
+    public void launch_TLM_4(View v){
+        Intent i = new Intent(this, tlm_intro_4.class);
         startActivity(i);
     }
 
-    public void launch_TLM_5(View a){
-        Intent i = new Intent(this, tlm_intro_5.class);
+    public void launch_TLM_6(View a){
+        Intent i = new Intent(this, tlm_intro_6.class);
         startActivity(i);
     }
 }
